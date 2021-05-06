b39a9e005042b865a1f33f9bcf02803fa11c5804
Joshua-Salvador <424joshuae.b.s@gmail.com>
2021-05-06 18:19:56 +0800

    Separate file for classes

--- index.js
+++ index.js
@@ -1,35 +1,60 @@
-const player = {
-  won: false,
-  positions: [false, false, false, false, false, false, false, false, false],
-  lastMove: null,
+const box0 = document.getElementById("0");
+const box1 = document.getElementById("1");
+const box2 = document.getElementById("2");
+const box3 = document.getElementById("3");
+const box4 = document.getElementById("4");
+const box5 = document.getElementById("5");
+const box6 = document.getElementById("6");
+const box7 = document.getElementById("7");
+const box8 = document.getElementById("8");
 
-  selectBox(box) {
-    if (this.positions[box]) return null;
-    if (bot.positions[box]) return null;
-    this.positions[box] = true;
-    this.lastMove = box;
+const player = new Player();
+const ai = new AI();
 
-    // call bot's select box method
-  },
+box0.addEventListener("click", () => {
+  console.log(`Testing Box ${box0.id}`);
+  player.selectBox(box0.id, ai);
+  ai.makeMove(player);
+});
+box1.addEventListener("click", () => {
+  console.log(`Testing Box ${box1.id}`);
+  player.selectBox(box1.id, ai);
+  ai.makeMove(player);
+});
+box2.addEventListener("click", () => {
+  console.log(`Testing Box ${box2.id}`);
+  player.selectBox(box2.id, ai);
+  ai.makeMove(player);
+});
+box3.addEventListener("click", () => {
+  console.log(`Testing Box ${box3.id}`);
+  player.selectBox(box3.id, ai);
+  ai.makeMove(player);
+});
+box4.addEventListener("click", () => {
+  console.log(`Testing Box ${box4.id}`);
+  player.selectBox(box4.id, ai);
+  ai.makeMove(player);
+});
+box5.addEventListener("click", () => {
+  console.log(`Testing Box ${box5.id}`);
+  player.selectBox(box5.id, ai);
+  ai.makeMove(player);
+});
+box6.addEventListener("click", () => {
+  console.log(`Testing Box ${box6.id}`);
+  player.selectBox(box6.id, ai);
+  ai.makeMove(player);
+});
+box7.addEventListener("click", () => {
+  console.log(`Testing Box ${box7.id}`);
+  player.selectBox(box7.id, ai);
+  ai.makeMove(player);
+});
+box8.addEventListener("click", () => {
+  console.log(`Testing Box ${box8.id}`);
+  player.selectBox(box8.id, ai);
+  ai.makeMove(player);
+});
 
-  /* Select Box Method
-  
-    get dom element
-    insert "X" in selected dom element
-   */
-
-  /* Check if Win
-
-    
-  */
-};
-
-const bot = {
-  won: false,
-  positions: [false, false, false, false, false, false, false, false, false],
-  lastMove: null,
-
-  // bot's select box method
-
-  // check if win
-};
+function selectBox(boxId) {}
