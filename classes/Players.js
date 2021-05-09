d8341f158f3f6aae64715c7c21587048a86fc4ca
Joshua-Salvador <424joshuae.b.s@gmail.com>
2021-05-09 18:12:52 +0800

    Second AI Move

--- classes/Players.js
+++ classes/Players.js
@@ -2,6 +2,7 @@ class Player {
   constructor() {
     this.won = false;
     this.positions = [null, null, null, null, null, null, null, null, null];
+    this.prevMove = null;
     this.lastMove = null;
     this.nthMove = "0";
   }
@@ -12,6 +13,7 @@ class Player {
     if (this.positions[box]) return null;
     if (bot.positions[box]) return null;
     this.positions[box] = true;
+    this.prevMove = this.lastMove;
     this.lastMove = box;
     this.nthMove = String(++this.nthMove);
     console.log(this);
@@ -52,6 +54,63 @@ class AI {
         }
         break;
       case "2":
+        if (Number(player.lastMove) % 2 !== 0) {
+          switch (true) {
+            case (player.prevMove === "0" && player.lastMove === "1") ||
+              (player.prevMove === "8" && player.lastMove === "3"):
+              this.positions[2] = true;
+              this.lastMove = "2";
+              break;
+            case (player.prevMove === "0" && player.lastMove === "5") ||
+              (player.prevMove === "8" && player.lastMove === "7"):
+              this.positions[6] = true;
+              this.lastMove = "6";
+              break;
+            case (player.prevMove === "2" && player.lastMove === "1") ||
+              (player.prevMove === "6" && player.lastMove === "5"):
+              this.positions[0] = true;
+              this.lastMove = "0";
+              break;
+            case (player.prevMove === "2" && player.lastMove === "3") ||
+              (player.prevMove === "6" && player.lastMove === "7"):
+              this.positions[8] = true;
+              this.lastMove = "8";
+              break;
+            default:
+              break;
+          }
+        } else {
+          switch (true) {
+            case (player.prevMove === "0" && player.prevMove === "2") ||
+              player.prevMove === "2" ||
+              player.lastMove === "0":
+              this.positions[1] = true;
+              this.lastMove = "1";
+              break;
+            case (player.prevMove === "2" && player.prevMove === "8") ||
+              player.prevMove === "8" ||
+              player.lastMove === "2":
+              this.positions[3] = true;
+              this.lastMove = "3";
+              break;
+            case (player.prevMove === "0" && player.prevMove === "6") ||
+              player.prevMove === "6" ||
+              player.lastMove === "0":
+              this.positions[5] = true;
+              this.lastMove = "5";
+              break;
+            case (player.prevMove === "6" && player.prevMove === "8") ||
+              player.prevMove === "8" ||
+              player.lastMove === "6":
+              this.positions[7] = true;
+              this.lastMove = "7";
+              break;
+            default:
+              this.positions[1] = true;
+              this.lastMove = "1";
+              break;
+          }
+        }
         break;
       case "3":
         break;
